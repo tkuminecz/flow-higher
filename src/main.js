b984adedb81b46a97ba0ae7cca8b29ed1103dbfb
Tim Kuminecz <tkuminecz@gmail.com>
2017-02-01 01:48:28 -0500

    add $App type

--- src/main.js
+++ src/main.js
@@ -48,6 +48,8 @@ export type $Union<L> = $_Union<*, *, L>
 
 export type $Tag<Label, Data> = { tag: Label, val: $TupleMap<Data, <V>(v: V) => V> }
 
+export type $App<K, T> = $Subtype<Type<K, T, *>>
+
 export class Type<Kind, Types, Data> {
 
 	/**
